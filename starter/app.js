c74ce6b535658af2ab434b4c4ccdaf4c30ff37fa
Sam Moore <swmoore84@gmail.com>
2020-05-30 23:44:17 +0900

    created methods to read input data

--- starter/app.js
+++ starter/app.js
@@ -3,17 +3,43 @@ var budgetController = (function() {
 
 })();
 
+
 var UIController = (function() {
+
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBTN: '.add__btn'
+  }
+
+  return {
+    getInput: function(){
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      };
+    },
+
+    getDOMstrings: function(){
+      return DOMstrings;
+    }
+  };
 
 })();
 
+
 var controller = (function(budgetCtrl, UICtrl) {
 
-  var ctrlAddItem = function(){
-    console.log("test")
+  var DOM = UICtrl.getDOMstrings();
+
+  var ctrlAddItem = function() {
+    var input = UICtrl.getInput();
+    console.log(input);
   }
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  document.querySelector(DOM.inputBTN).addEventListener('click', ctrlAddItem);
 
   document.addEventListener('keypress', function(event) {
     if (event.keyCode === 13){
