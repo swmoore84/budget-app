6ce7505ae70344b77dd5528032cda8b4fb23c5d5
Sam Moore <swmoore84@gmail.com>
2020-05-30 15:30:02 +0900

    created modules for budget app

---
+++ starter/app.js
@@ -0,0 +1,31 @@
+var budgetController = (function() {
+
+  var x = 23;
+
+  var add = function(a) {
+    return x + a;
+  }
+
+  return {
+    publicTest: function(b) {
+      return add(b);
+    }
+  }
+
+})();
+
+var UIController = (function() {
+
+})();
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+  var z = budgetController.publicTest(5);
+
+  return {
+    anotherPublic: function() {
+      console.log(z
+    }
+  }
+
+})(budgetController, UIController);
